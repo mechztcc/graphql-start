e4288e0f30b78ac2b1170e6e598b4d1c88e8e204
mechztcc <dojathan@gmail.com>
2023-03-25 11:02:38 -0300

    feat: resolvers to User

--- src/index.js
+++ src/index.js
@@ -4,14 +4,50 @@ const { startStandaloneServer } = require("@apollo/server/standalone");
 
 const typeDefs = buildSchema(`
 
-type Query {
-    hello: String!
-  }`);
+    type User {
+        id: ID!
+        name: String!
+        second_name: String!
+        full_name: String!
+        email: String!
+        birth: Int!
+        payment: Float!
+        vip: Boolean!
+        createdAt: String!
+        createdDate: String!
+    }
+
+    type Query {
+        hello: String!
+        userByName: User!
+    }
+`);
 
 const resolvers = {
+  User: {
+    createdDate(parent) {
+      return new Date(parent.createdAt).toISOString();
+    },
+    full_name(parent) {
+      return `${parent.name} ${parent.second_name}`;
+    },
+  },
+
   Query: {
     hello() {
       return new Date().toDateString();
+    },
+    userByName(parent, args, contextValue, info) {
+      return {
+        id: 1,
+        name: "alberto",
+        second_name: "paiva",
+        email: "email@email.com",
+        birth: "22/10/2022",
+        payment: 5000.0,
+        vip: true,
+        createdAt: new Date().toDateString(),
+      };
     },
   },
 };
